5e310f0fbd242a5422bdfa1719a18d15128d63af
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-06-19 18:00:49 +0330

    .

--- e-commerce-next/my-app/components/categories/categories.jsx
+++ e-commerce-next/my-app/components/categories/categories.jsx
@@ -1,9 +1,33 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 export default function Categories({ categories }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  function handleClick(id) {
+    const params = new URLSearchParams(searchParams);
+    params.delete("page");
+    params.set("category", id);
+    router.replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <ul>
       {categories.map((item) => {
         return (
-          <li className="my-2 cursor-pointer " key={item.id}>
+          <li
+            className={
+              searchParams.has("category") &&
+              searchParams.get("category") == item.id
+                ? "filter-list-active"
+                : "my-2 cursor-pointer"
+            }
+            key={item.id}
+            onClick={() => {
+              handleClick(item.id);
+            }}
+          >
             {item.name}
           </li>
         );
